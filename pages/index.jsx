e3f0e2bf9576b6f2c8ca160b992a5ae61c2fe9a5
davidle.codes <davidl.ruizc@gmail.com>
2019-03-05 17:15:00 -0500

    Adding the React + Next project

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class extends Component {
+  
+    render() {
+    return (
+      <div>
+        <header>CoffeeCast</header>
+        <div className="channels">
+            {
+
+            }
+        </div>
+      </div>
+    )
+  }
+}
