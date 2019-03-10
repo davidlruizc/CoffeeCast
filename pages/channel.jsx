cf664e0d33e8aa07fb3d650d1bdbfa630ccbc33e
davidle.codes <davidl.ruizc@gmail.com>
2019-03-10 16:43:12 -0500

    Refactoring channel component

--- pages/channel.jsx
+++ pages/channel.jsx
@@ -4,44 +4,44 @@ import ChannelPlaylist from '../components/ChannelPlaylist'
 import Error from 'next/error'
 
 export default class extends Component {
-    // fetching the data from a channel
-    static async getInitialProps({query, res}){
-      try {
-        // Getting the id from the channel
-        let channelId = query.id 
+  // fetching the data from a channel
+  static async getInitialProps({query, res}){
+    try {
+      // Getting the id from the channel
+      let channelId = query.id 
 
-        // Parallel request
-        let [ reqChannel, reqAudio, reqSeries ] = await Promise.all([
-            fetch(`https://api.audioboom.com/channels/${channelId}`),
-            fetch(`https://api.audioboom.com/channels/${channelId}/audio_clips`),
-            fetch(`https://api.audioboom.com/channels/${channelId}/child_channels`)
-        ])
-        
-        // Handling status 404
-        if(reqChannel.status >= 400){
-          res.statusCode = reqChannel.status
-          return{ channel: null, audioClip: null, series: null, statusCode: reqChannel.status }
-        }
-
-        let dataChannel = await reqChannel.json()
-        let channel = dataChannel.body.channel
-
-        // audio_clips is going to list the last podcast of the channel        
-        let dataAudio = await reqAudio.json()
-        let audioClip = dataAudio.body.audio_clips
-
-        // if a channel has subchannels 
-        let dataSeries = await reqSeries.json()
-        let series = dataSeries.body.channels
-
-        return { channel, audioClip, series, statusCode: 200 } 
+      // Parallel request
+      let [ reqChannel, reqAudio, reqSeries ] = await Promise.all([
+          fetch(`https://api.audioboom.com/channels/${channelId}`),
+          fetch(`https://api.audioboom.com/channels/${channelId}/audio_clips`),
+          fetch(`https://api.audioboom.com/channels/${channelId}/child_channels`)
+      ])
+      
+      // Handling status 404
+      if(reqChannel.status >= 400){
+        res.statusCode = reqChannel.status
+        return{ channel: null, audioClip: null, series: null, statusCode: reqChannel.status }
       }
-      catch(e){
-        // Set the status code the get from the server
-        res.statusCode = 503
-        return{ channel: null, audioClip: null, series: null, statusCode: 503 }
-      }
+
+      let dataChannel = await reqChannel.json()
+      let channel = dataChannel.body.channel
+
+      // audio_clips is going to list the last podcast of the channel        
+      let dataAudio = await reqAudio.json()
+      let audioClip = dataAudio.body.audio_clips
+
+      // if a channel has subchannels 
+      let dataSeries = await reqSeries.json()
+      let series = dataSeries.body.channels
+
+      return { channel, audioClip, series, statusCode: 200 } 
     }
+    catch(e){
+      // Set the status code the get from the server
+      res.statusCode = 503
+      return{ channel: null, audioClip: null, series: null, statusCode: 503 }
+    }
+  }
 
   render() {
     const { channel, audioClip, series, statusCode } = this.props  
