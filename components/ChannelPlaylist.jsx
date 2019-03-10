01e184e2d4e56950a49333a2deba24b642ef41b7
davidle.codes <davidl.ruizc@gmail.com>
2019-03-10 16:44:27 -0500

    Adding the state for the podcast player first part

--- components/ChannelPlaylist.jsx
+++ components/ChannelPlaylist.jsx
@@ -1,115 +1,108 @@
 import React, { Component } from 'react'
-import {Link} from '../routes'
 import GriidChannels from './GriidChannels';
-import slug from '../helpers/slug'
+import PodcastListWithPlayer from './PodcastListWithPlayer';
 
  export default class ChannelPlaylist extends Component {
-     render() {
-        const { channel, audioClip, series } = this.props
-          return(
-              <React.Fragment>
-                  <div 
-                    className="banner" 
-                    style={{ backgroundImage: `url(${ channel.urls.banner_image.original })` }}
-                  />
-                    <React.Fragment>
-                    {
-                        series.length > 0 &&
-                        <div>
-                            <h2 className="subtitles">Series</h2>
-                            <GriidChannels
-                              channels={series}
-                            />
-                      </div>
-                    }
-                    </React.Fragment>
-                    <React.Fragment>
-                      {
-                        audioClip.length > 0 &&
-                          <React.Fragment>
-                            <h2 className="subtitles">Ultimos Podcast</h2>
-                            {
-                              audioClip.map(clip => (
-                                <Link
-                                prefetch
-                                route="podcast"
-                                params={{
-                                  slug: slug(clip.title),
-                                  id: clip.id,
-                                  slugChannel: slug(clip.channel.title),
-                                  idChannel: clip.channel.id
-                                }}
-                                key={clip.id}
-                                >
-                                  <a className="podcast">
-                                      <h3>
-                                        { clip.title }
-                                      </h3>
-                                      <div className="meta">
-                                        {
-                                            // Returns the smallest integer greater than or equal to its numeric argument.
-                                            Math.ceil( clip.duration / 60)
-                                        } minutos
-                                      </div>
-                                  </a>
-                                </Link>
-                              ))
-                            }
-                          </React.Fragment>
-                      }
-                    </React.Fragment>
-                <style jsx>
-                  {`
-                    .banner {
-                        width: 100%;
-                        padding-bottom: 25%;
-                        background-position: 50% 50%;
-                        background-size: cover;
-                        background-color: #aaa;
-                    }
-                    h1 {
-                        font-weight: 600;
-                        padding: 15px;
-                    }
-                    h2 {
-                        padding: 5px;
-                        font-size: 0.9em;
-                        font-weight: 600;
-                        margin: 0;
-                        text-align: center;
-                    }
-                    .subtitles {
-                        padding: 5px;
-                        font-size: 22px;
-                        font-weight: 400;
-                        color: rgb(92, 97, 102);
-                        line-height: 1.4;
-                        margin: 0;
-                        text-align: center;
-                    }
+  constructor(props){
+    super(props)
+    // State for the podcast modal
+    this.state = { openPodcast: null }
+  }
 
-                    .podcast {
-                        display: block;
-                        text-decoration: none;
-                        color: #333;
-                        padding: 15px;
-                        border-bottom: 1px solid rgba(0,0,0,0.2);
-                        cursor: pointer;
-                    }
-                    .podcast:hover {
-                        color: #000;
-                    }
-                    .podcast h3 {
-                        margin: 0;
-                    }
-                    .podcast .meta {
-                        color: #666;
-                        margin-top: 0.5em;
-                        font-size: 0.8em;
-                    }
-                  `}
-                </style>
+  openPodcast = (event, podcast) => {
+    // Remove all the native states
+    event.preventDefault()
+    this.setState({
+      openPodcast: podcast
+    })
+  }
+
+  render() {
+    const { channel, audioClip, series } = this.props
+    const { openPodcast } = this.state
+
+    return(
+      <React.Fragment>
+        { openPodcast && <div>Hay un podcast :3</div> }
+        <div 
+          className="banner" 
+          style={{ backgroundImage: `url(${ channel.urls.banner_image.original })` }}
+        />
+        <React.Fragment>
+        {
+          series.length > 0 &&
+          <div>
+              <h2 className="subtitles">Series</h2>
+              <GriidChannels
+                channels={series}
+              />
+          </div>
+        }
+        </React.Fragment>
+        <React.Fragment>
+          {
+            audioClip.length > 0 &&
+              <React.Fragment>
+                <h2 className="subtitles">Ultimos Podcast</h2>
+                <PodcastListWithPlayer
+                  podcasts={audioClip}
+                  onClickPodcast={this.openPodcast}
+                />
               </React.Fragment>
-          )
-     }
+          }
+        </React.Fragment>
+        <style jsx>
+          {`
+            .banner {
+                width: 100%;
+                padding-bottom: 25%;
+                background-position: 50% 50%;
+                background-size: cover;
+                background-color: #aaa;
+            }
+            h1 {
+                font-weight: 600;
+                padding: 15px;
+            }
+            h2 {
+                padding: 5px;
+                font-size: 0.9em;
+                font-weight: 600;
+                margin: 0;
+                text-align: center;
+            }
+            .subtitles {
+                padding: 5px;
+                font-size: 22px;
+                font-weight: 400;
+                color: rgb(92, 97, 102);
+                line-height: 1.4;
+                margin: 0;
+                text-align: center;
+            }
+
+            .podcast {
+                display: block;
+                text-decoration: none;
+                color: #333;
+                padding: 15px;
+                border-bottom: 1px solid rgba(0,0,0,0.2);
+                cursor: pointer;
+            }
+            .podcast:hover {
+                color: #000;
+            }
+            .podcast h3 {
+                margin: 0;
+            }
+            .podcast .meta {
+                color: #666;
+                margin-top: 0.5em;
+                font-size: 0.8em;
+            }
+          `}
+        </style>
+      </React.Fragment>
+    )
+  }
  }
